fd19e8f234bd266fced400c64b67c0235e61978c
nipaaa <akternipa941@gmail.com>
2023-10-23 15:05:11 +0600

    update

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,7 +7,7 @@ import HowItWorks from '../../components/howItWorks/HowItWorks'
 import WhatPeopleSay from '../../components/whatPeopleSay/WhatPeopleSay'
 import Trusted from '../../components/trusted/Trusted'
 import Footer from '../../components/footer/Footer'
-import Accordion from '../../components/Accordion/Accordion'
+import Accordion from '../../components/accordion/Accordion'
 
 const Home = () => {
   return (
