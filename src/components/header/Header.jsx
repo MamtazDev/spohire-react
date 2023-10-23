7d869f3f0abc96012bfadad0e5314eccc5c3af3f
tonni-akter-dev <tanniakhter123@gmail.com>
2023-10-23 16:11:09 +0600

    login form design

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,49 +1,70 @@
-import React from 'react'
-import './Header.css'
-import Logo from '../../assets/logo.png';
-import { Image } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import "./Header.css";
+import Logo from "../../assets/logo.png";
+import { Image } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-    return (
-        <header>
-            <nav class="navbar navbar-expand-lg">
-                <div class="container">
-                    <a class="navbar-brand" href="index.html">
-                        <Image src={Logo} alt="Logo" />
-                    </a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                        aria-expanded="false" aria-label="Toggle navigation">
-                        <i class="fa fa-bars"></i>
-                        {/* <span class="navbar-toggler-icon"></span> */}
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                                <Link class="nav-link" href="#">Transfer Market</Link>
-                            </li>
-                            <li class="nav-item">
-                                <Link class="nav-link" href="#">Job Offers</Link>
-                            </li>
-                            <li class="nav-item">
-                                <Link class="nav-link" href="#">Announcements</Link>
-                            </li>
-                            <li class="nav-item">
-                                <Link class="nav-link" href="#">Pricing</Link>
-                            </li>
-                        </ul>
-                        <form class="d-flex flex-column flex-lg-row align-items-center gap-2">
-                            <button class="logIn">Log in</button>
-                            <button class="authBtn btnNone">
-                                <Link href="#signUp" type="submit" className='text-decoration-none'>Sign Up</Link>
-                            </button>
-                        </form>
-                    </div>
-                </div>
-            </nav>
-        </header>
-    )
-}
+  return (
+    <header>
+      <nav className="navbar navbar-expand-lg">
+        <div className="container">
+          <a className="navbar-brand" href="index.html">
+            <Image src={Logo} alt="Logo" />
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <i className="fa fa-bars"></i>
+            {/* <span className="navbar-toggler-icon"></span> */}
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link className="nav-link" to="#">
+                  Transfer Market
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="#">
+                  Job Offers
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="#">
+                  Announcements
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="#">
+                  Pricing
+                </Link>
+              </li>
+            </ul>
+            <form className="d-flex flex-column flex-lg-row align-items-center gap-2">
+              <button className="logIn">
+                <Link to="/login">Log in</Link>
+              </button>
+              <button className="authBtn btnNone">
+                <Link
+                  to="/signup"
+                  type="submit"
+                  classNameName="text-decoration-none"
+                >
+                  Sign Up
+                </Link>
+              </button>
+            </form>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css'
-import './responsive.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import Home from './pages/Home/Home.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import "./responsive.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home/Home.jsx";
+import Login from "./Authentication/Login/Login";
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <Home />
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path:'/login',
+    element:<Login/>
   }
-])
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
