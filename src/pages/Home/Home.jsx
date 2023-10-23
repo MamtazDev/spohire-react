c0a44c99ce738d734ec3c3e85d6b18b231dc6d30
nipaaa <akternipa941@gmail.com>
2023-10-23 13:28:52 +0600

    accordion add

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,6 +7,7 @@ import HowItWorks from '../../components/howItWorks/HowItWorks'
 import WhatPeopleSay from '../../components/whatPeopleSay/WhatPeopleSay'
 import Trusted from '../../components/trusted/Trusted'
 import Footer from '../../components/footer/Footer'
+import Accordion from '../../components/Accordion/Accordion'
 
 const Home = () => {
   return (
@@ -21,6 +22,7 @@ const Home = () => {
         <HowItWorks />
         <WhatPeopleSay />
         <Trusted />
+        <Accordion/>
       </main>
       <Footer />
     </>
