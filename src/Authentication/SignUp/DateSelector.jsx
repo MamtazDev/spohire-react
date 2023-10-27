3f1f86d5e6b34e19f9d66df4e0e8592a363351f2
tonni-akter-dev <tanniakhter123@gmail.com>
2023-10-27 15:50:03 +0600

    select added

--- src/Authentication/SignUp/DateSelector.jsx
+++ src/Authentication/SignUp/DateSelector.jsx
@@ -1,135 +1,82 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
+// eslint-disable-next-line no-unused-vars
+import arrow from "../../assets/CaretDown.png";
+import "./SignUp.css";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const DateSelector = () => {
-  const today = new Date();
-  const daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-  const [selectedDate, setSelectedDate] = useState(today);
-  const [years, setYears] = useState(5);
-  const [dayOptions, setDayOptions] = useState([]);
-  const [monthOptions, setMonthOptions] = useState([]);
-  const [yearOptions, setYearOptions] = useState([]);
+  const currentDate = new Date();
+
+  const [myMonth, setMyMonth] = useState(currentDate);
+  const [myYear, setMyYear] = useState(currentDate);
+  const [myDay, setMyDay] = useState(currentDate);
+
+  const minDate = new Date(myYear.getFullYear(), myMonth.getMonth(), 1);
+  const maxDate = new Date(myYear.getFullYear(), myMonth.getMonth() + 1, 0);
 
   useEffect(() => {
-    setYearsInDropdown(years);
-    initializeMonthOptions();
-  }, [years]);
+    setMyDay(new Date(myYear.getFullYear(), myMonth.getMonth(), 1));
+  }, [myMonth, myYear, setMyDay]);
 
-  useEffect(() => {
-    setDays(selectedDate.getMonth());
-  }, [selectedDate]);
-
-  const setYearsInDropdown = (val) => {
-    const year = today.getFullYear();
-    const yearOptions = [];
-    yearOptions.push(
-      <option key="year-placeholder" value="">
-        Year
-      </option>
-    );
-    for (let i = 0; i < val; i++) {
-      yearOptions.push(
-        <option key={year + i} value={year + i}>
-          {year + i}
-        </option>
-      );
+  const renderDayContents = (day, date) => {
+    if (date < minDate || date > maxDate) {
+      return <span></span>;
     }
-    setYearOptions(yearOptions);
+    return <span>{date.getDate()}</span>;
   };
-
-  const initializeMonthOptions = () => {
-    const monthOptions = [];
-    monthOptions.push(
-      <option key="month-placeholder" value="">
-        Month
-      </option>
-    );
-    for (let i = 0; i < 12; i++) {
-      monthOptions.push(
-        <option key={i} value={i}>
-          {getMonthName(i)}
-        </option>
-      );
-    }
-    setMonthOptions(monthOptions);
-  };
-
-  const setDays = (monthIndex) => {
-    const daysCount = daysInMonth[monthIndex];
-    const dayOptions = [];
-    dayOptions.push(
-      <option key="day-placeholder" value="">
-        Day
-      </option>
-    );
-    for (let i = 0; i < daysCount; i++) {
-      dayOptions.push(
-        <option key={i + 1} value={i + 1}>
-          {i + 1}
-        </option>
-      );
-    }
-    setDayOptions(dayOptions);
-  };
-
-  const handleMonthChange = (e) => {
-    const monthIndex = parseInt(e.target.value, 10);
-    if (monthIndex >= 0) {
-      setDays(monthIndex);
-    }
-    const newDate = new Date(selectedDate);
-    newDate.setMonth(monthIndex);
-    setSelectedDate(newDate);
-  };
-
-  const handleDayChange = (e) => {
-    const day = parseInt(e.target.value, 10);
-    if (day >= 1) {
-      const newDate = new Date(selectedDate);
-      newDate.setDate(day);
-      setSelectedDate(newDate);
-    }
-  };
-
-  const handleYearChange = (e) => {
-    const year = parseInt(e.target.value, 10);
-    if (year >= today.getFullYear()) {
-      const newDate = new Date(selectedDate);
-      newDate.setFullYear(year);
-      setSelectedDate(newDate);
-    }
-  };
-
-  const getMonthName = (monthIndex) => {
-    const monthNames = [
-      'January',
-      'February',
-      'March',
-      'April',
-      'May',
-      'June',
-      'July',
-      'August',
-      'September',
-      'October',
-      'November',
-      'December',
-    ];
-    return monthNames[monthIndex];
-  };
-
+  
   return (
-    <div className="select-date">
-      <select id="select-day" className='select_date'  value={selectedDate.getDate()} onChange={handleDayChange}>
-        {dayOptions}
-      </select>
-      <select id="select-month" className='select_date'  value={selectedDate.getMonth()} onChange={handleMonthChange}>
-        {monthOptions}
-      </select>
-      <select id="select-year"  className='select_date' value={selectedDate.getFullYear()} onChange={handleYearChange}>
-        {yearOptions}
-      </select>
-    </div>
+    <>
+      <div className="d-flex align-items-center gap-3">
+        <div className="year_input">
+          <DatePicker
+            selected={myYear}
+            onChange={(date) => setMyYear(date)}
+            showYearPicker
+            dateFormat="yyyy"
+          />
+        </div>
+
+        <div className="month_input">
+          <DatePicker
+            showMonthYearPicker
+            dateFormat="MMMM"
+            selected={myMonth}
+            onChange={(date) => setMyMonth(date)}
+          />
+        </div>
+
+        <div className="day">
+          <DatePicker
+            dateFormat="dd"
+            selected={myDay}
+            renderDayContents={renderDayContents}
+            onChange={(date) => setMyDay(date)}
+          />
+        </div>
+      </div>
+    </>
   );
 };
 
 export default DateSelector;
+
+{
+  /* <select
+id="select-month"
+className="select_date"
+value={selectedDate.getMonth()}
+onChange={handleMonthChange}
+>
+{monthOptions}
+</select>
+<select
+id="select-year"
+className="select_date"
+value={selectedDate.getFullYear()}
+onChange={handleYearChange}
+>
+{yearOptions}
+</select> */
+}
--- src/Authentication/SignUp/SignUp.jsx
+++ src/Authentication/SignUp/SignUp.jsx
@@ -1,22 +1,236 @@
+/* eslint-disable no-unused-vars */
 import loginBanner from "../../assets/login-banner.png";
 import arrowRight from "../../assets/ArrowRight.png";
-
 import { useState } from "react";
-import ReactFlagsSelect from "react-flags-select";
 import DateSelector from "./DateSelector";
+import Select from "react-select";
+import bdIcon from "../../assets/bd.svg";
+import "./SignUp.css";
 
-// const options = [
-//   { value: "1", label: "1" },
-//   { value: "2", label: "2" },
-//   { value: "3", label: "3" },
-//   { value: "4", label: "4" },
-//   { value: "5", label: "5" },
-//   { value: "6", label: "6" },
-//   { value: "7", label: "7" },
-//   { value: "8", label: "8" },
-//   { value: "9", label: "9" },
-//   { value: "10", label: "10" },
-// ];
+const options = [
+  {
+    value: "1",
+    label: "1",
+    flagImg: `
+  <svg width="20" height="15" viewBox="0 0 20 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g clip-path="url(#clip0_274_437)">
+<path d="M0.9375 0.666046H19.3302V14.4605H0.9375V0.666046Z" fill="#006A4E"/>
+<path d="M10.1341 11.985C12.4783 11.985 14.3786 10.0847 14.3786 7.74059C14.3786 5.39643 12.4783 3.49612 10.1341 3.49612C7.78996 3.49612 5.88965 5.39643 5.88965 7.74059C5.88965 10.0847 7.78996 11.985 10.1341 11.985Z" fill="#F42A41"/>
+</g>
+<defs>
+<clipPath id="clip0_274_437">
+<rect x="0.9375" y="0.666046" width="18.3927" height="13.7945" rx="2.82964" fill="white"/>
+</clipPath>
+</defs>
+</svg>
+`,
+  },
+  {
+    value: "2",
+    label: "2",
+    flagImg: `
+  <svg width="20" height="15" viewBox="0 0 20 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g clip-path="url(#clip0_274_437)">
+<path d="M0.9375 0.666046H19.3302V14.4605H0.9375V0.666046Z" fill="#006A4E"/>
+<path d="M10.1341 11.985C12.4783 11.985 14.3786 10.0847 14.3786 7.74059C14.3786 5.39643 12.4783 3.49612 10.1341 3.49612C7.78996 3.49612 5.88965 5.39643 5.88965 7.74059C5.88965 10.0847 7.78996 11.985 10.1341 11.985Z" fill="#F42A41"/>
+</g>
+<defs>
+<clipPath id="clip0_274_437">
+<rect x="0.9375" y="0.666046" width="18.3927" height="13.7945" rx="2.82964" fill="white"/>
+</clipPath>
+</defs>
+</svg>
+`,
+  },
+  {
+    value: "3",
+    label: "3",
+    flagImg: `
+  <svg width="20" height="15" viewBox="0 0 20 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g clip-path="url(#clip0_274_437)">
+<path d="M0.9375 0.666046H19.3302V14.4605H0.9375V0.666046Z" fill="#006A4E"/>
+<path d="M10.1341 11.985C12.4783 11.985 14.3786 10.0847 14.3786 7.74059C14.3786 5.39643 12.4783 3.49612 10.1341 3.49612C7.78996 3.49612 5.88965 5.39643 5.88965 7.74059C5.88965 10.0847 7.78996 11.985 10.1341 11.985Z" fill="#F42A41"/>
+</g>
+<defs>
+<clipPath id="clip0_274_437">
+<rect x="0.9375" y="0.666046" width="18.3927" height="13.7945" rx="2.82964" fill="white"/>
+</clipPath>
+</defs>
+</svg>
+`,
+  },
+  {
+    value: "4",
+    label: "4",
+    flagImg: `
+  <svg width="20" height="15" viewBox="0 0 20 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g clip-path="url(#clip0_274_437)">
+<path d="M0.9375 0.666046H19.3302V14.4605H0.9375V0.666046Z" fill="#006A4E"/>
+<path d="M10.1341 11.985C12.4783 11.985 14.3786 10.0847 14.3786 7.74059C14.3786 5.39643 12.4783 3.49612 10.1341 3.49612C7.78996 3.49612 5.88965 5.39643 5.88965 7.74059C5.88965 10.0847 7.78996 11.985 10.1341 11.985Z" fill="#F42A41"/>
+</g>
+<defs>
+<clipPath id="clip0_274_437">
+<rect x="0.9375" y="0.666046" width="18.3927" height="13.7945" rx="2.82964" fill="white"/>
+</clipPath>
+</defs>
+</svg>
+`,
+  },
+  {
+    value: "5",
+    label: "5",
+    flagImg: `
+  <svg width="20" height="15" viewBox="0 0 20 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g clip-path="url(#clip0_274_437)">
+<path d="M0.9375 0.666046H19.3302V14.4605H0.9375V0.666046Z" fill="#006A4E"/>
+<path d="M10.1341 11.985C12.4783 11.985 14.3786 10.0847 14.3786 7.74059C14.3786 5.39643 12.4783 3.49612 10.1341 3.49612C7.78996 3.49612 5.88965 5.39643 5.88965 7.74059C5.88965 10.0847 7.78996 11.985 10.1341 11.985Z" fill="#F42A41"/>
+</g>
+<defs>
+<clipPath id="clip0_274_437">
+<rect x="0.9375" y="0.666046" width="18.3927" height="13.7945" rx="2.82964" fill="white"/>
+</clipPath>
+</defs>
+</svg>
+`,
+  },
+  {
+    value: "6",
+    label: "6",
+    flagImg: `
+  <svg width="20" height="15" viewBox="0 0 20 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g clip-path="url(#clip0_274_437)">
+<path d="M0.9375 0.666046H19.3302V14.4605H0.9375V0.666046Z" fill="#006A4E"/>
+<path d="M10.1341 11.985C12.4783 11.985 14.3786 10.0847 14.3786 7.74059C14.3786 5.39643 12.4783 3.49612 10.1341 3.49612C7.78996 3.49612 5.88965 5.39643 5.88965 7.74059C5.88965 10.0847 7.78996 11.985 10.1341 11.985Z" fill="#F42A41"/>
+</g>
+<defs>
+<clipPath id="clip0_274_437">
+<rect x="0.9375" y="0.666046" width="18.3927" height="13.7945" rx="2.82964" fill="white"/>
+</clipPath>
+</defs>
+</svg>
+`,
+  },
+  {
+    value: "7",
+    label: "7",
+    flagImg: `
+  <svg width="20" height="15" viewBox="0 0 20 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g clip-path="url(#clip0_274_437)">
+<path d="M0.9375 0.666046H19.3302V14.4605H0.9375V0.666046Z" fill="#006A4E"/>
+<path d="M10.1341 11.985C12.4783 11.985 14.3786 10.0847 14.3786 7.74059C14.3786 5.39643 12.4783 3.49612 10.1341 3.49612C7.78996 3.49612 5.88965 5.39643 5.88965 7.74059C5.88965 10.0847 7.78996 11.985 10.1341 11.985Z" fill="#F42A41"/>
+</g>
+<defs>
+<clipPath id="clip0_274_437">
+<rect x="0.9375" y="0.666046" width="18.3927" height="13.7945" rx="2.82964" fill="white"/>
+</clipPath>
+</defs>
+</svg>
+`,
+  },
+  {
+    value: "8",
+    label: "8",
+    flagImg: `
+  <svg width="20" height="15" viewBox="0 0 20 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g clip-path="url(#clip0_274_437)">
+<path d="M0.9375 0.666046H19.3302V14.4605H0.9375V0.666046Z" fill="#006A4E"/>
+<path d="M10.1341 11.985C12.4783 11.985 14.3786 10.0847 14.3786 7.74059C14.3786 5.39643 12.4783 3.49612 10.1341 3.49612C7.78996 3.49612 5.88965 5.39643 5.88965 7.74059C5.88965 10.0847 7.78996 11.985 10.1341 11.985Z" fill="#F42A41"/>
+</g>
+<defs>
+<clipPath id="clip0_274_437">
+<rect x="0.9375" y="0.666046" width="18.3927" height="13.7945" rx="2.82964" fill="white"/>
+</clipPath>
+</defs>
+</svg>
+`,
+  },
+  {
+    value: "9",
+    label: "9",
+    flagImg: `
+  <svg width="20" height="15" viewBox="0 0 20 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g clip-path="url(#clip0_274_437)">
+<path d="M0.9375 0.666046H19.3302V14.4605H0.9375V0.666046Z" fill="#006A4E"/>
+<path d="M10.1341 11.985C12.4783 11.985 14.3786 10.0847 14.3786 7.74059C14.3786 5.39643 12.4783 3.49612 10.1341 3.49612C7.78996 3.49612 5.88965 5.39643 5.88965 7.74059C5.88965 10.0847 7.78996 11.985 10.1341 11.985Z" fill="#F42A41"/>
+</g>
+<defs>
+<clipPath id="clip0_274_437">
+<rect x="0.9375" y="0.666046" width="18.3927" height="13.7945" rx="2.82964" fill="white"/>
+</clipPath>
+</defs>
+</svg>
+`,
+  },
+  {
+    value: "10",
+    label: "10",
+    flagImg: `
+  <svg width="20" height="15" viewBox="0 0 20 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g clip-path="url(#clip0_274_437)">
+<path d="M0.9375 0.666046H19.3302V14.4605H0.9375V0.666046Z" fill="#006A4E"/>
+<path d="M10.1341 11.985C12.4783 11.985 14.3786 10.0847 14.3786 7.74059C14.3786 5.39643 12.4783 3.49612 10.1341 3.49612C7.78996 3.49612 5.88965 5.39643 5.88965 7.74059C5.88965 10.0847 7.78996 11.985 10.1341 11.985Z" fill="#F42A41"/>
+</g>
+<defs>
+<clipPath id="clip0_274_437">
+<rect x="0.9375" y="0.666046" width="18.3927" height="13.7945" rx="2.82964" fill="white"/>
+</clipPath>
+</defs>
+</svg>
+`,
+  },
+];
+
+const options1 = [
+  {
+    value: "Bangladesh",
+    label: "Bangladesh",
+    flagImg: bdIcon,
+  },
+  {
+    value: "India",
+    label: "India",
+    flagImg: bdIcon,
+  },
+  {
+    value: "Switzerland",
+    label: "Switzerland",
+    flagImg: bdIcon,
+  },
+  {
+    value: "United Kingdom",
+    label: "United Kingdom",
+    flagImg: bdIcon,
+  },
+  {
+    value: "Ireland",
+    label: "Ireland",
+    flagImg: bdIcon,
+  },
+  {
+    value: "Italy",
+    label: "Italy",
+    flagImg: bdIcon,
+  },
+  {
+    value: "Netherland",
+    label: "Netherland",
+    flagImg: bdIcon,
+  },
+];
+
+const options2 = [
+  {
+    value: "Bangladesh",
+    label: "+880",
+    flagImg: bdIcon,
+  },
+  {
+    value: "India",
+    label: "+880",
+    flagImg: bdIcon,
+  },
+];
+
 const SignUp = () => {
   const [select, setSelect] = useState("SE");
   const onSelect = (code) => setSelect(code);
@@ -30,6 +244,7 @@ const SignUp = () => {
               <img style={{ width: "100%" }} src={loginBanner} alt="" />
             </div>
           </div>
+
           <div className="col-lg-7">
             <div className="login_wrapper">
               <div>
@@ -37,8 +252,9 @@ const SignUp = () => {
                 <p>Fill all input to create a account</p>
                 <form>
                   <div className="row right-inner-addon input-container">
+                  <label htmlFor="">Full name</label>
                     <div className="col-lg-6">
-                      <label htmlFor="">Full name</label>
+                      
 
                       <input
                         type="text"
@@ -47,14 +263,6 @@ const SignUp = () => {
                       />
                     </div>
                     <div className="col-lg-6">
-                      <label
-                        htmlFor=""
-                        className=""
-                        style={{ visibility: "hidden" }}
-                      >
-                        {" "}
-                        sdfffffffffffffffffffff
-                      </label>
                       <input
                         type="text"
                         className="form-control login_input"
@@ -71,19 +279,143 @@ const SignUp = () => {
                       placeholder="Enter email"
                     />
                   </div>
-                  <div className="row flex-wrap date_wrapper">
-                    <div className="col-lg-4">
-                      <ReactFlagsSelect
-                        selected={select}
-                        onSelect={onSelect}
-                        countries={["BD", "fi", "GB", "IE", "IT", "NL", "SE"]}
+                  <div className="row flex-wrap date_wrapper mb-3">
+                    <div className="col-lg-6">
+                      <label htmlFor="">Nationality</label>
+                      <Select
+                        options={options1}
+                        formatOptionLabel={(countryFlag) => (
+                          <div className="d-flex align-items-center justify-content-between">
+                            <span>{countryFlag.label}</span>
+                            <img
+                              src={countryFlag.flagImg}
+                              alt="country-image"
+                            />
+                          </div>
+                        )}
+                        styles={{
+                          control: (baseStyles) => ({
+                            ...baseStyles,
+                            minHeight: "0px",
+                            backgroundColor: "#FAFAFA",
+                          }),
+
+                          container: (baseStyles) => ({
+                            ...baseStyles,
+                            width: "100%",
+                          }),
+
+                          valueContainer: (baseStyles) => ({
+                            ...baseStyles,
+                            padding: "0 5px",
+                          }),
+                          placeholder: (baseStyles) => ({
+                            ...baseStyles,
+                            color: "#9CA3A9",
+                            fontSize: "10px",
+                          }),
+                          menuList: (baseStyles) => ({
+                            ...baseStyles,
+                            fontSize: "10px",
+                          }),
+                          singleValue: (baseStyles) => ({
+                            ...baseStyles,
+                            fontSize: "10px",
+                          }),
+                          indicatorsContainer: (baseStyles) => ({
+                            ...baseStyles,
+                            padding: "0px !important",
+                          }),
+                          indicatorSeparator: (baseStyles) => ({
+                            ...baseStyles,
+                            display: "none",
+                            margin: "0",
+                            width: "0",
+                          }),
+
+                          // indicatorsContainer: (baseStyles) =>
+                        }}
                       />
                     </div>
-                    <div className="col-lg-8">
+
+                    <div className="col-lg-6">
+                      <label htmlFor="">Date of birth</label>
                       <DateSelector />
                     </div>
                   </div>
                   {/* country select */}
+                  {/* email input */}
+                  <div className="row email_input">
+                    <label htmlFor="">Phone number</label>
+                    <div className="col-lg-4 mb-3">
+                      <Select
+                        placeholder="+880"
+                        options={options2}
+                        formatOptionLabel={(countryFlag) => (
+                          <div className="d-flex align-items-center justify-content-between">
+                            <span>{countryFlag.label}</span>
+                            <img
+                              src={countryFlag.flagImg}
+                              alt="country-image"
+                            />
+                          </div>
+                        )}
+                        styles={{
+                          control: (baseStyles) => ({
+                            ...baseStyles,
+                            minHeight: "0px",
+                            backgroundColor: "#FAFAFA",
+                          }),
+
+                          container: (baseStyles) => ({
+                            ...baseStyles,
+                            width: "100%",
+                          }),
+
+                          valueContainer: (baseStyles) => ({
+                            ...baseStyles,
+                            padding: "0 5px",
+                          }),
+                          placeholder: (baseStyles) => ({
+                            ...baseStyles,
+                            color: "#9CA3A9",
+                            fontSize: "10px",
+                          }),
+                          menuList: (baseStyles) => ({
+                            ...baseStyles,
+                            fontSize: "10px",
+                          }),
+                          singleValue: (baseStyles) => ({
+                            ...baseStyles,
+                            fontSize: "10px",
+                          }),
+                          indicatorsContainer: (baseStyles) => ({
+                            ...baseStyles,
+                            padding: "0px !important",
+                          }),
+                          indicatorSeparator: (baseStyles) => ({
+                            ...baseStyles,
+                            display: "none",
+                            margin: "0",
+                            width: "0",
+                          }),
+
+                          // indicatorsContainer: (baseStyles) =>
+                        }}
+                      />
+                    </div>
+                    <div className="col-lg-8">
+                      <div className="phone_number">
+                        <div></div>
+                        <input
+                          type="number"
+                          className="ph_number"
+                          placeholder="0123 456 7890 "
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  {/* email input end */}
                   {/* Function */}
                   <label className="label_text">Function</label> <br />
                   <div>
