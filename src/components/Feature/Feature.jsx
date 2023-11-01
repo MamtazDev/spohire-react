f66580ab529f707a7b6d6d1022e8e84834c0a419
zerin22 <tajrin.ini@gmail.com>
2023-11-01 12:35:10 +0600

    Feature image fix

--- src/components/Feature/Feature.jsx
+++ src/components/Feature/Feature.jsx
@@ -28,24 +28,24 @@ const Feature = () => {
                     <div className="row gy-3 justify-content-center">
                         <div className="col-12 col-md-6 col-xl-4">
                             <div className="featureBoxInner h-100" style={{ background: `url(${featureBg1})` }}>
-                                <div className="row align-items-center p-0 pe-0 ">
-                                    <div className="col-6">
+                                <div className="row align-items-end p-0 h-100 ">
+                                    <div className="col-6 p-0">
                                         <h4 className="mb-1">Confidentiality</h4>
                                         <p>I can tell you first hand, ChainGPT is absolutely amazing. They have helped
                                             me
                                             and my team speed up our working process by 125%. The access to blockchain
                                             information was never easier.</p>
                                     </div>
-                                    <div className="col-6 feature_card" >
-                                        <img className="" src={FeatureOne} alt="img" />
+                                    <div className="col-6 feature_card p-0" >
+                                        <img className="img-fluid w-100" src={FeatureOne} alt="img" />
                                     </div>
                                 </div>
                             </div>
                         </div>
                         <div className="col-12 col-md-6 col-xl-4">
                             <div className="featureBoxInner h-100" style={{ background: `url(${featureBg2})` }}>
-                                <div className="row align-items-center p-0">
-                                    <div className="col-6">
+                                <div className="row align-items-end p-0 h-100">
+                                    <div className="col-6 p-0">
                                         <h4 className="mb-1">Stay in Touch</h4>
                                         <p>
                                             I can tell you first hand, ChainGPT is absolutely amazing. They have
@@ -54,15 +54,15 @@ const Feature = () => {
                                             blockchain information was never easier.
                                         </p>
                                     </div>
-                                    <div className="col-6 feature_card">
-                                            <img className="" src={FeatureTwo} alt="img" />
+                                    <div className="col-6 feature_card p-0">
+                                        <img className="img-fluid w-100" src={FeatureTwo} alt="img" />
                                     </div>
                                 </div>
                             </div>
                         </div>
                         <div className="col-12 col-md-6 col-xl-4 ">
                             <div className="featureBoxInner h-100" style={{ background: `url(${featureBg3})` }}>
-                                <div className="row align-items-center p-0">
+                                <div className="row align-items-end p-0 h-100">
                                     <div className="col-6">
                                         <h4 className="mb-1">Confidentiality</h4>
                                         <p>I can tell you first hand, ChainGPT is absolutely amazing. They have
@@ -71,8 +71,8 @@ const Feature = () => {
                                             blockchain
                                             information was never easier.</p>
                                     </div>
-                                    <div className="col-6 feature_card">
-                                        <img className="" src={FeatureThree} alt="img" />
+                                    <div className="col-6 feature_card p-0">
+                                        <img className="img-fluid w-100" src={FeatureThree} alt="img" />
                                     </div>
                                 </div>
                             </div>
