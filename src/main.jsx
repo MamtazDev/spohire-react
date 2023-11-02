c6d2689b6ecbc8606890aa2954ece336625e01e7
zerin22 <tajrin.ini@gmail.com>
2023-11-02 15:36:39 +0600

    Update all

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
-import './responsive.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import TransfarMarket from './pages/TransfarMarket';
