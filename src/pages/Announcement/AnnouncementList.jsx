9c65b1eb0c729cf53bea05e0990862075a9468f9
tonni-akter-dev <tanniakhter123@gmail.com>
2023-11-22 17:23:19 +0600

    announcement page done

--- src/pages/Announcement/AnnouncementList.jsx
+++ src/pages/Announcement/AnnouncementList.jsx
@@ -1,9 +1,315 @@
+import a1 from '../../assets/a1.png';
+import a2 from '../../assets/a2.png';
+import a3 from '../../assets/a3.png';
+import a4 from '../../assets/a4.png';
+import location from '../../assets/location.png';
+import plus2 from '../../assets/plus2.png';
+import flag from '../../assets/flag.png';
+import edit2 from '../../assets/edit2.png';
+import bookmark from '../../assets/bookmark.png';
+import delet from '../../assets/delete.png';
+import dollar from '../../assets/coin-dollar.png';
 
 const AnnouncementList = () => {
     return (
         <div>
-            <div className="container">
+            <div className="container" style={{ marginTop: "104px" }}>
+                <div className="row">
+                    <div className="col-lg-9">
+                        <div className="announcelist_wrapper">
+                            {/* parent flex with edit delete  */}
+                            <div className='d-flex justify-content-between align-items-start'>
+                                <div className="d-flex" style={{ gap: "20px" }}>
+                                    <div className='announcement_pic'>
+                                        <img src={a1} alt="" />
+                                    </div>
+                                    <div className='recruiment f_sfPro'>
+                                        <p>Player recruitment</p>
+                                        <div className="d-flex gap-3">
+                                            <div className='d-flex align-items-center' style={{ gap: "6px" }}>
+                                                <img src={location} alt="" />
+                                                <span>Vegas Street Circuit</span>
+                                            </div>
+                                            <div className='d-flex align-items-center' style={{ gap: "6px" }}>
+                                                <img src={flag} alt="" />
+                                                <span>Vegas Street Circuit</span>
+                                            </div>
+                                            <div className='d-flex align-items-center' style={{ gap: "6px" }}>
+                                                <img src={dollar} alt="" />
+                                                <span>Vegas Street Circuit</span>
+                                            </div>
+                                        </div>
 
+                                    </div>
+                                </div>
+
+                                {/* icon div */}
+                                <div className='d-flex gap-3'>
+                                    <img src={bookmark} alt="" />
+                                    <img src={edit2} alt="" />
+                                    <img src={delet} alt="" />
+                                </div>
+                            </div>
+                            <p className='announcement_details f_sfPro'>We cant wait to share this milestone with our incredible sports community. Your passion and support have fueled our journey, and were thrilled to <br /> take it to the next level together.</p>
+                        </div>
+                        <div className="announcelist_wrapper">
+                            {/* parent flex with edit delete  */}
+                            <div className='d-flex justify-content-between align-items-start'>
+                                <div className="d-flex" style={{ gap: "20px" }}>
+                                    <div className='announcement_pic'>
+                                        <img src={a1} alt="" />
+                                    </div>
+                                    <div className='recruiment f_sfPro'>
+                                        <p>Player recruitment</p>
+                                        <div className="d-flex gap-3">
+                                            <div className='d-flex align-items-center' style={{ gap: "6px" }}>
+                                                <img src={location} alt="" />
+                                                <span>Vegas Street Circuit</span>
+                                            </div>
+                                            <div className='d-flex align-items-center' style={{ gap: "6px" }}>
+                                                <img src={flag} alt="" />
+                                                <span>Vegas Street Circuit</span>
+                                            </div>
+                                            <div className='d-flex align-items-center' style={{ gap: "6px" }}>
+                                                <img src={dollar} alt="" />
+                                                <span>Vegas Street Circuit</span>
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </div>
+
+                                {/* icon div */}
+                                <div className='d-flex gap-3'>
+                                    <img src={bookmark} alt="" />
+                                    <img src={edit2} alt="" />
+                                    <img src={delet} alt="" />
+                                </div>
+                            </div>
+                            <p className='announcement_details f_sfPro'>We cant wait to share this milestone with our incredible sports community. Your passion and support have fueled our journey, and were thrilled to <br /> take it to the next level together.</p>
+                        </div>
+                        <div className="announcelist_wrapper">
+                            {/* parent flex with edit delete  */}
+                            <div className='d-flex justify-content-between align-items-start'>
+                                <div className="d-flex" style={{ gap: "20px" }}>
+                                    <div className='announcement_pic'>
+                                        <img src={a1} alt="" />
+                                    </div>
+                                    <div className='recruiment f_sfPro'>
+                                        <p>Player recruitment</p>
+                                        <div className="d-flex gap-3">
+                                            <div className='d-flex align-items-center' style={{ gap: "6px" }}>
+                                                <img src={location} alt="" />
+                                                <span>Vegas Street Circuit</span>
+                                            </div>
+                                            <div className='d-flex align-items-center' style={{ gap: "6px" }}>
+                                                <img src={flag} alt="" />
+                                                <span>Vegas Street Circuit</span>
+                                            </div>
+                                            <div className='d-flex align-items-center' style={{ gap: "6px" }}>
+                                                <img src={dollar} alt="" />
+                                                <span>Vegas Street Circuit</span>
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </div>
+
+                                {/* icon div */}
+                                <div className='d-flex gap-3'>
+                                    <img src={bookmark} alt="" />
+                                    <img src={edit2} alt="" />
+                                    <img src={delet} alt="" />
+                                </div>
+                            </div>
+                            <p className='announcement_details f_sfPro'>We cant wait to share this milestone with our incredible sports community. Your passion and support have fueled our journey, and were thrilled to <br /> take it to the next level together.</p>
+                        </div>
+                        <div className="announcelist_wrapper">
+                            {/* parent flex with edit delete  */}
+                            <div className='d-flex justify-content-between align-items-start'>
+                                <div className="d-flex" style={{ gap: "20px" }}>
+                                    <div className='announcement_pic'>
+                                        <img src={a1} alt="" />
+                                    </div>
+                                    <div className='recruiment f_sfPro'>
+                                        <p>Player recruitment</p>
+                                        <div className="d-flex gap-3">
+                                            <div className='d-flex align-items-center' style={{ gap: "6px" }}>
+                                                <img src={location} alt="" />
+                                                <span>Vegas Street Circuit</span>
+                                            </div>
+                                            <div className='d-flex align-items-center' style={{ gap: "6px" }}>
+                                                <img src={flag} alt="" />
+                                                <span>Vegas Street Circuit</span>
+                                            </div>
+                                            <div className='d-flex align-items-center' style={{ gap: "6px" }}>
+                                                <img src={dollar} alt="" />
+                                                <span>Vegas Street Circuit</span>
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </div>
+
+                                {/* icon div */}
+                                <div className='d-flex gap-3'>
+                                    <img src={bookmark} alt="" />
+                                    <img src={edit2} alt="" />
+                                    <img src={delet} alt="" />
+                                </div>
+                            </div>
+                            <p className='announcement_details f_sfPro'>We cant wait to share this milestone with our incredible sports community. Your passion and support have fueled our journey, and were thrilled to <br /> take it to the next level together.</p>
+                        </div>
+                        <div className="announcelist_wrapper">
+                            {/* parent flex with edit delete  */}
+                            <div className='d-flex justify-content-between align-items-start'>
+                                <div className="d-flex" style={{ gap: "20px" }}>
+                                    <div className='announcement_pic'>
+                                        <img src={a1} alt="" />
+                                    </div>
+                                    <div className='recruiment f_sfPro'>
+                                        <p>Player recruitment</p>
+                                        <div className="d-flex gap-3">
+                                            <div className='d-flex align-items-center' style={{ gap: "6px" }}>
+                                                <img src={location} alt="" />
+                                                <span>Vegas Street Circuit</span>
+                                            </div>
+                                            <div className='d-flex align-items-center' style={{ gap: "6px" }}>
+                                                <img src={flag} alt="" />
+                                                <span>Vegas Street Circuit</span>
+                                            </div>
+                                            <div className='d-flex align-items-center' style={{ gap: "6px" }}>
+                                                <img src={dollar} alt="" />
+                                                <span>Vegas Street Circuit</span>
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </div>
+
+                                {/* icon div */}
+                                <div className='d-flex gap-3'>
+                                    <img src={bookmark} alt="" />
+                                    <img src={edit2} alt="" />
+                                    <img src={delet} alt="" />
+                                </div>
+                            </div>
+                            <p className='announcement_details f_sfPro'>We cant wait to share this milestone with our incredible sports community. Your passion and support have fueled our journey, and were thrilled to <br /> take it to the next level together.</p>
+                        </div>
+                        <div className="announcelist_wrapper">
+                            {/* parent flex with edit delete  */}
+                            <div className='d-flex justify-content-between align-items-start'>
+                                <div className="d-flex" style={{ gap: "20px" }}>
+                                    <div className='announcement_pic'>
+                                        <img src={a1} alt="" />
+                                    </div>
+                                    <div className='recruiment f_sfPro'>
+                                        <p>Player recruitment</p>
+                                        <div className="d-flex gap-3">
+                                            <div className='d-flex align-items-center' style={{ gap: "6px" }}>
+                                                <img src={location} alt="" />
+                                                <span>Vegas Street Circuit</span>
+                                            </div>
+                                            <div className='d-flex align-items-center' style={{ gap: "6px" }}>
+                                                <img src={flag} alt="" />
+                                                <span>Vegas Street Circuit</span>
+                                            </div>
+                                            <div className='d-flex align-items-center' style={{ gap: "6px" }}>
+                                                <img src={dollar} alt="" />
+                                                <span>Vegas Street Circuit</span>
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </div>
+
+                                {/* icon div */}
+                                <div className='d-flex gap-3'>
+                                    <img src={bookmark} alt="" />
+                                    <img src={edit2} alt="" />
+                                    <img src={delet} alt="" />
+                                </div>
+                            </div>
+                            <p className='announcement_details f_sfPro'>We cant wait to share this milestone with our incredible sports community. Your passion and support have fueled our journey, and were thrilled to <br /> take it to the next level together.</p>
+                        </div>
+
+
+                    </div>
+                    <div className="col-lg-3">
+                        <div className="category_wrapper">
+                            <div className="blue_cat">
+                                <p>Job by category</p>
+                            </div>
+                            <div className='white_category_part'>
+                                <div className="cat_list">
+                                    <div className='blue_square'></div>
+                                    <span>Football</span>
+                                </div>
+                                <div className="cat_list">
+                                    <div className='blue_square'></div>
+                                    <span>Basketball</span>
+                                </div>
+                                <div className="cat_list">
+                                    <div className='blue_square'></div>
+                                    <span>Handball</span>
+                                </div>
+                                <div className="cat_list">
+                                    <div className='blue_square'></div>
+                                    <span>Volleyball</span>
+                                </div>
+
+                                <div className='cat_list1'>
+                                    <div className='blue_plus'>
+                                        <img src={plus2} alt="plus" />
+                                    </div>
+                                    <p>Show more</p>
+                                </div>
+                            </div>
+
+                        </div>
+                        <div className="category_wrapper">
+                            <div className="blue_cat">
+                                <p>Job by location</p>
+                            </div>
+                            <div className='white_category_part'>
+                                <div className="cat_list">
+                                    <div className='blue_square'></div>
+                                    <span>Paris</span>
+                                </div>
+                                <div className="cat_list">
+                                    <div className='blue_square'></div>
+                                    <span>London</span>
+                                </div>
+                                <div className="cat_list">
+                                    <div className='blue_square'></div>
+                                    <span>Finland</span>
+                                </div>
+                                <div className="cat_list">
+                                    <div className='blue_square'></div>
+                                    <span>Canada</span>
+                                </div>
+
+                                <div className='cat_list1'>
+                                    <div className='blue_plus'>
+                                        <img src={plus2} alt="plus" />
+                                    </div>
+                                    <p>Show more</p>
+                                </div>
+                            </div>
+
+                        </div>
+
+
+
+                        <div className='best_matched'>
+                            <h3>Get Best Matched Jobs On your Email</h3>
+                            <button className='apply_now_btn'>Apply Now</button>
+                        </div>
+
+
+                    </div>
+
+                </div>
 
 
             </div>
