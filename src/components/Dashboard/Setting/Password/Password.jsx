c5a61bf55fe4f69e35fde65de232de7dece57e0e
tonni-akter-dev <tanniakhter123@gmail.com>
2023-11-23 16:00:30 +0600

    notification page design done

--- src/components/Dashboard/Setting/Password/Password.jsx
+++ src/components/Dashboard/Setting/Password/Password.jsx
@@ -1,9 +1,34 @@
-
+import './Password.css';
+import lock from '../../../../assets/lock.png'
 const Password = () => {
     return (
-        <div>
-            
-        </div>
+        <form style={{ paddingTop: "76px" }}>
+            <div className="pass_wrapper">
+
+                <div className="form-group has-search">
+                    <label htmlFor="">Old password</label>
+                    <span className=" form-control-feedback1"><img src={lock} alt="" /></span>
+                    <input type="text" className="form-control" placeholder="Password" />
+                </div>
+                <div className="form-group has-search">
+                    <label htmlFor="">New password</label>
+                    <span className=" form-control-feedback"><img src={lock} alt="" /></span>
+                    <input type="text" className="form-control" placeholder="New password" />
+                    <p>Minimum 8 characters</p>
+                </div>
+                <div className="form-group has-search">
+                    <label htmlFor="">Confirm new password</label>
+                    <span className=" form-control-feedback"><img src={lock} alt="" /></span>
+                    <input type="text" className="form-control" placeholder="Confirm new password" />
+                    <p>Minimum 8 characters</p>
+                </div>
+                <button className='save_btn'>Save Changes</button>
+
+
+
+
+            </div>
+        </form>
     );
 };
 
