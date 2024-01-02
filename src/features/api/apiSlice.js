058f5671b8fe062b844e59e4ee189348a60bbb1a
mohi14 <mohiuddinkhan1409@gmail.com>
2024-01-02 16:35:19 +0600

    api urll ddd

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -2,7 +2,10 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.env.VITE_API_URL,
+    baseUrl:
+      process.env.NODE_ENV !== "production"
+        ? import.meta.env.VITE_LOCAL_API_URL
+        : import.meta.env.VITE_LIVE_API_URL,
     prepareHeaders: async (headers, { getState, endpoints }) => {
       const token = getState()?.auth?.accessToken;
       if (token) {
