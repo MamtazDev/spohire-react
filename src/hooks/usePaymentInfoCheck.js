3d98d6100d7e1080b9706a639cc74a496cf0647e
mohi14 <mohiuddinkhan1409@gmail.com>
2024-01-05 14:44:39 +0600

    plan selection fixing

--- src/hooks/usePaymentInfoCheck.js
+++ src/hooks/usePaymentInfoCheck.js
@@ -2,9 +2,13 @@ import { useSelector } from "react-redux";
 
 export const usePaymentInfoCheck = () => {
   const { packageInfo } = useSelector((state) => state.payment);
-  if (packageInfo?.name) {
-    return true;
+  const { user } = useSelector((state) => state.auth);
+  if (packageInfo?.name && user) {
+    return { status: true };
+  }
+  if (!user) {
+    return { status: false, path: "/login" };
   } else {
-    return false;
+    return { status: false, path: "/pricing" };
   }
 };
