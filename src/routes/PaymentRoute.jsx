19cb8fb9f41f8dd9783307e628bbd1e9237390b4
mohi14 <mohiuddinkhan1409@gmail.com>
2024-01-06 16:59:49 +0600

    some issue fixed

--- src/routes/PaymentRoute.jsx
+++ src/routes/PaymentRoute.jsx
@@ -1,13 +1,14 @@
 import React from "react";
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import { usePaymentInfoCheck } from "../hooks/usePaymentInfoCheck";
 
 const PaymentRoute = ({ children }) => {
   const isPackageSelected = usePaymentInfoCheck();
+  const location = useLocation();
   return isPackageSelected?.status ? (
     children
   ) : (
-    <Navigate to={isPackageSelected?.path} />
+    <Navigate to={isPackageSelected?.path} state={{ from: location }} replace />
   );
 };
 
