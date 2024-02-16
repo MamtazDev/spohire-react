31e06b98ad95b0547c91d309b98d1ac8b6fbbf17
muhammadashiqurrahman <getintouch.ashiq@gmail.com>
2024-02-16 09:56:17 +0600

    ::update

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,18 +1,19 @@
+import { Outlet } from "react-router-dom";
 import Footer from "../components/footer/Footer";
 import Header from "../components/header/Header";
 
-const Layout = ({children}) => {
-    return (
-        <>
-            {/* <div className={`${location.pathname == "/" ? "" : "bgBanner"}`}> */}
-            <div className="bgBanner">
-                <Header/>
-            </div>
-            {/*<Outlet/>*/}
-            {children}
-            <Footer/>
-        </>
-    );
+const Layout = ({ children }) => {
+  return (
+    <>
+      {/* <div className={`${location.pathname == "/" ? "" : "bgBanner"}`}> */}
+      <div className="bgBanner">
+        <Header />
+      </div>
+      <Outlet />
+      {children}
+      <Footer />
+    </>
+  );
 };
 
 export default Layout;
